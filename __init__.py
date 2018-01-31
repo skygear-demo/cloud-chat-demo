e080aedff4447e417be9233dd6be2278d16732cf
Ho Wa <howa@oursky.com>
2018-01-31 13:54:42 +0800

    Update Python Cloud Code
    Connects SkygearIO/guides#166

--- __init__.py
+++ __init__.py
@@ -1,23 +1,14 @@
 from skygear.transmitter.encoding import deserialize_record
-from chat.hooks import *
+from skygear.action import push_users
 from skygear.container import SkygearContainer
 from skygear.options import options as skyoptions
-from skygear.utils.context import current_context, current_user_id
-
-def _get_container():
-    return SkygearContainer(api_key=skyoptions.masterkey,
-                            user_id=current_user_id())
-
-
-def __send_to_user(user_ids, title, message):
-    container = _get_container()
-    n = {'gcm': {'notification': {'title': title, 'body': message}}}
-    payload = {'user_ids': user_ids, 'notification': n}
-    print(container.send_action('push:user', payload))
-
+from skygear.utils.context import current_user_id
+from chat.decorators import *
 
 @after_message_sent
 def after_message_sent_hook(message, conversation, participants):
+    container = SkygearContainer(api_key=skyoptions.masterkey,
+                                 user_id=current_user_id())
     message = deserialize_record(message)
     conversation = deserialize_record(conversation)
     participants = [deserialize_record(p) for p in participants]
@@ -28,4 +19,21 @@ def after_message_sent_hook(message, conversation, participants):
         content = current_user['username'] + ": " + message['body']
     else:
         content = current_user['username'] + " sent you a file."
-    __send_to_user(other_user_ids, conversation['title'], content)
+    notification = {'gcm': {
+                       'notification': {
+                           'title': conversation['title'],
+                           'body': content
+                       }
+                    },
+                    'apns': {
+                        'aps': {
+                            'alert': {
+                                'title': conversation['title'],
+                                'body': content
+                            },
+                            'from': 'skygear',
+                            'operation': 'notification'
+                        }
+                    }
+                   }
+    push_users(container, other_user_ids, notification)
